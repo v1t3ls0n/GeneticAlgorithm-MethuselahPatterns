c9089f35da98b671df71d9c51d60d95e71326410
v1t3ls0n <guyvitelson@gmail.com>
2024-12-26 07:23:46 +0200

    FIXED PROBLEM! GREAT update

--- main.py
+++ main.py
@@ -28,6 +28,6 @@ def main(grid_size, population_size, generations, mutation_rate, alive_cells_wei
 
 
 # Example call to main function
-main(grid_size=5, population_size=50, generations=300, mutation_rate=0.2,
+main(grid_size=10, population_size=50, generations=300, mutation_rate=0.2,
      alive_cells_weight=0.2, lifespan_weight=1000, alive_growth_weight=0.3, stableness_weight = 0.1,
      alive_cells_per_block=5, alive_blocks=1)
